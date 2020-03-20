95c7e35551ae697894db17bcd7cca4ad381bbb89
ReformedCola <reformedcola@gmail.com>
2020-03-20 11:17:39 -0700

    init

---
+++ jason.com/public/jason.js
@@ -0,0 +1,48 @@
+function ajax(method, url) {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+    request.open(method, url);
+    request.onreadystatechange = () => {
+      if (request.readyState === 4) {
+        if (request.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request);
+        }
+      }
+    };
+    request.send();
+  });
+}
+ajax("get", "http://qq.com:8888/friends.json").then(response => {
+  console.log('AJAX & CORS');
+  console.log(response);
+});
+
+function jsonp(url) {
+  return new Promise((resolve, reject) => {
+    const random = 'jasonJSONPCallbackName' + Math.random()
+    window[random] = (data) => {
+      resolve(data)
+    }
+    const script = document.createElement('script')
+    script.src = `${url}?callback=${random}`
+    script.onload = () => {
+      script.remove()
+    }
+    script.onerror = () => {
+      reject()
+    }
+    document.body.appendChild(script)
+  })
+}
+
+jsonp('http://qq.com:8888/friends.js')
+  .then((data) => {
+    console.log('JSONP')
+    console.log(data)
+  })
+
+
+
+
